72f9359862d135a20c230c574fc5c42444c080b6
inomata724 <inomata724@gmail.com>
2021-04-27 22:45:11 +0900

    refactor

--- lib/sidekiq_graceful_shutdown/sidekiq.rb
+++ lib/sidekiq_graceful_shutdown/sidekiq.rb
@@ -1,6 +1,45 @@
 module SidekiqGracefulShutdown
   class Sidekiq
     class << self
+      def shutdown
+        interval_sec = 5.seconds
+        hostname     = Socket.gethostname
+
+        quit_processes_by_hostname(hostname)
+
+        loop do
+          processes = find_processes_by_hostname(hostname)
+          if processes.all? { |process| process['quiet'] == 'true' && process['busy'] == 0 }
+            processes.each do |process|
+              begin
+                Process.kill('TERM', process['pid'])
+              rescue Errno::ESRCH
+              end
+            end
+
+            break
+          end
+
+          sleep interval_sec
+        end
+      end
+
+      def prepare_shutdown
+        interval_sec = 5.seconds
+        hostname     = Socket.gethostname
+
+        quit_processes_by_hostname(hostname)
+
+        loop do
+          processes = find_processes_by_hostname(hostname)
+          if processes.all? { |process| process['quiet'] == 'true' && process['busy'] == 0 }
+            break
+          end
+
+          sleep interval_sec
+        end
+      end
+
       def find_processes_by_hostname(hostname)
         ::Sidekiq::ProcessSet.new.to_a.select { |p| p['hostname'] == hostname }
       end
